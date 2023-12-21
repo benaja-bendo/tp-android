db3c01780ad3f7f24b28e7045fee4325ce9d9436
benaja-bendo <benaja.bendo02@gmail.com>
2023-12-21 11:35:14 +0100

    update HomeActivity.kt

--- app/src/main/java/com/example/tp_epsi/HomeActivity.kt
+++ app/src/main/java/com/example/tp_epsi/HomeActivity.kt
@@ -8,6 +8,14 @@ import android.widget.Toolbar
 import com.google.android.material.appbar.AppBarLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import okhttp3.CacheControl
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,13 +25,46 @@ class HomeActivity : AppCompatActivity() {
 //        val appBar = findViewById<AppBarLayout>(R.id.appBar)
 //        appBar.addView(Toolbar(this))
 
-        val category= arrayListOf<Category>()
-        val recyclerViewCategory= findViewById<RecyclerView>(R.id.recyclerViewCategory)
-        recyclerViewCategory.layoutManager= LinearLayoutManager(this)
-        val categoryAdapter=CategoryAdapter(category)
-        recyclerViewCategory.adapter=categoryAdapter
-        val progressBar=findViewById<ProgressBar>(R.id.progressBar)
-        progressBar.visibility=View.VISIBLE
+        val category = arrayListOf<Category>()
+        val recyclerViewCategory = findViewById<RecyclerView>(R.id.recyclerViewCategory)
+        recyclerViewCategory.layoutManager = LinearLayoutManager(this)
+        val categoryAdapter = CategoryAdapter(category)
+        recyclerViewCategory.adapter = categoryAdapter
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        progressBar.visibility = View.VISIBLE
+
+        val okHttpClient: OkHttpClient = OkHttpClient.Builder().build()
+        val mRequestUrl = "https://ugarit.online/epsi/categories.json"
+        val request =
+            Request.Builder().url(mRequestUrl).get().cacheControl(CacheControl.FORCE_NETWORK)
+                .build()
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val data = response.body()?.string()
+                if (data != null) {
+                    val jsStudents = JSONObject(data)
+                    val jsArrayStudents = jsStudents.getJSONArray("items")
+                    for (i in 0 until jsArrayStudents.length()) {
+                        val js = jsArrayStudents.getJSONObject(i)
+                        val student = Category(
+                            js.optString("products_url", ""),
+                            js.optString("category_id", ""),
+                            js.optString("title", "le titre"),
+                        )
+                        category.add(student)
+                    }
+                    runOnUiThread {
+                        categoryAdapter.notifyDataSetChanged()
+                        progressBar.visibility = View.GONE
+                    }
+
+                }
+            }
+        })
 
     }
 }
